35da487ff1e302a1d3875fb4b30166bcf97936da
reenmayee <158175578+reenmayee@users.noreply.github.com>
2026-05-31 03:02:02 +0530

    chunking.py

--- processing/chunking.py
+++ processing/chunking.py
@@ -1,12 +1,8 @@
-import nltk
+def create_chunks(text, chunk_size=500):
 
-def create_chunks(text, chunk_size=3):
-    sentences = nltk.sent_tokenize(text)
-    
     chunks = []
-    
-    for i in range(0, len(sentences), chunk_size):
-        chunk = " ".join(sentences[i:i+chunk_size])
-        chunks.append(chunk)
-    
+
+    for i in range(0, len(text), chunk_size):
+        chunks.append(text[i:i + chunk_size])
+
     return chunks
