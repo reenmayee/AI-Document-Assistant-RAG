5a3ea55ab7c160e58bf54952674744c2a6710e75
reenmayee <158175578+reenmayee@users.noreply.github.com>
2026-05-31 03:08:11 +0530

    embedding.py

--- processing/embedding.py
+++ processing/embedding.py
@@ -1,6 +1,5 @@
 from sentence_transformers import SentenceTransformer
 
-# load model ONCE (important for performance)
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embeddings(chunks):
